b889b9e07560083e2f54c6ce6dcdc1266579c4e3
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-13 09:13:38 +0300

    handle error

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,3 +1,31 @@
-export default function Error(){
-    return <h3>An Error occurred</h3>
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+
+export default function Error() {
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong";
+
+  if (error.status === 200) {
+    const parsedData = JSON.parse(error.data);
+    if (parsedData && parsedData.message) {
+      message = parsedData.message;
+    }
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find resource or page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -19,7 +19,8 @@ export async function loader(){
 
     if (!response.ok) {
         // return {isError: true, message: 'An Error occurred'}
-        throw {message: 'An Error occurred'}
+        // throw {message: 'An Error occurred'}
+        throw new Response(JSON.stringify({message: 'Could not fetch events'}, {status: 500 }))
     } else {
         return response
     }
